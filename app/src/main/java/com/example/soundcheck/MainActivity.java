a7b309d42ced41101f8b2d5aa90327301f58f587
Devin Zhang <devinzhang415@gmail.com>
2022-04-18 23:45:40 -0400

    Added pulse animation

--- app/src/main/java/com/example/soundcheck/MainActivity.java
+++ app/src/main/java/com/example/soundcheck/MainActivity.java
@@ -9,9 +9,12 @@ import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -36,34 +39,32 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int VIBE_PERIOD = 200;
 
-    private static boolean isRecording = false;
+    private static FloatingActionButton fab;
+    private static ImageView imgAnim1, imgAnim2;
+    private static AudioRecord mic;
+    private static Vibrator vibe;
     private static Thread listeningThread;
+    private static Handler animHandler;
     private static final Object Lock = new Object();
+    private static boolean isRecording = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FloatingActionButton fab = findViewById(R.id.microphone_button);
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_CODE);
-        }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.VIBRATE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.VIBRATE}, REQUEST_CODE);
-        }
-
-        AudioRecord mic = new AudioRecord(AUDIO_SOURCE, SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT, BUFFER_SIZE);
-        Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        init();
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (isRecording) {
                     isRecording = false;
+                    animHandler.removeCallbacks(animRunnable);
                 } else {
                     isRecording = true;
                     mic.startRecording();
+                    animRunnable.run();
 
                     listeningThread = new Thread(new Runnable() {
                         public void run() {
@@ -97,6 +98,23 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void init() {
+        fab = findViewById(R.id.microphone_button);
+        imgAnim1 = findViewById(R.id.imgAnim1);
+        imgAnim2 = findViewById(R.id.imgAnim2);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_CODE);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.VIBRATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.VIBRATE}, REQUEST_CODE);
+        }
+
+        mic = new AudioRecord(AUDIO_SOURCE, SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT, BUFFER_SIZE);
+        vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        animHandler = new Handler();
+    }
+
     private double getVolume(AudioRecord mic, short[] buffer) {
         int read = mic.read(buffer, DATA_OFFSET, BUFFER_SIZE);
         int v = 0;
@@ -116,4 +134,29 @@ public class MainActivity extends AppCompatActivity {
         average /= AVERAGE_PERIOD;
         return average;
     }
+
+    private Runnable animRunnable = new Runnable() {
+        @Override
+        public void run() {
+            imgAnim1.animate().scaleX(4f).scaleY(4f).alpha(0f).setDuration(1000).withEndAction(new Runnable() {
+                @Override
+                public void run() {
+                    imgAnim1.setScaleX(1f);
+                    imgAnim1.setScaleY(1f);
+                    imgAnim1.setAlpha(1f);
+                }
+            });
+
+            imgAnim2.animate().scaleX(4f).scaleY(4f).alpha(0f).setDuration(1000).withEndAction(new Runnable() {
+                @Override
+                public void run() {
+                    imgAnim2.setScaleX(1f);
+                    imgAnim2.setScaleY(1f);
+                    imgAnim2.setAlpha(1f);
+                }
+            });
+
+            animHandler.postDelayed(animRunnable, 1500);
+        }
+    };
 }
